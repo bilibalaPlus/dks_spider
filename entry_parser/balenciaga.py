0c3e9915547b732741e106edf6a92c7fb40aeca9
yingl <linying_43151@163.com>
2017-06-26 15:19:33 +0800

    Update balenciaga.py

--- entry_parser/balenciaga.py
+++ entry_parser/balenciaga.py
@@ -11,5 +11,8 @@ def parse(driver, url):
     driver.get(url)
     elements = util.find_elements_by_css_selector(driver, 'a.item-display-image-container')
     for element in elements:
-        products.append(element.get_attribute('href'))
+        products.append(element.get_attribute('href').strip())
     return ';'.join(products)
+
+if __name__ == '__main__':
+    print(parse(util.create_chrome_driver(), sys.argv[1])
