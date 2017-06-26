5149acc5b8d7f46c396ea166399af77b7fa15b9f
yingl <linying_43151@163.com>
2017-06-26 14:36:56 +0800

    Create balenciaga.py

---
+++ entry_parser/balenciaga.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+import sys
+sys.path.append('../')
+import util
+
+prefix = 'www.balenciaga.cn'
+
+def parse(driver, url):
+    products = []
+    driver.get(url)
+    elements = util.find_elements_by_css_selector(driver, 'item-display-image-container')
+    for element in elements:
+        products.append(element.get_attribute('href')
+    return ';'.join(products)
