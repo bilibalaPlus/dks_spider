15dc679bf6588876e007d58aedb3eebb6a778955
yingl <linying_43151@163.com>
2017-06-26 12:08:19 +0800

    Update util.py

--- util.py
+++ util.py
@@ -3,6 +3,10 @@
 from selenium import webdriver
 import importlib
 import os
+import time
+
+def sleep(seconds = 1):
+    time.sleep(seconds)
 
 def create_chrome_driver():
     options = webdriver.ChromeOptions()
@@ -10,6 +14,18 @@ def create_chrome_driver():
     driver = webdriver.Chrome(chrome_options = options)
     driver.implicitly_wait(5)
     return driver
+
+def find_element_by_css_selector(item, selector):
+    try:
+        return item.find_element_by_css_selector(selector)
+    except:
+        return None
+        
+def find_elements_by_css_selector(item, selector):
+    try:
+        return item.find_elements_by_css_selector(selector)
+    except:
+        return []
 
 def load_parsers(folder, name = ''):
     if folder.endswith('/'):
