0403ebd12309f23505ee650b6fd0723ef4e207a4
yingl <linying_43151@163.com>
2017-06-26 14:56:33 +0800

    Update balenciaga.py

--- entry_parser/balenciaga.py
+++ entry_parser/balenciaga.py
@@ -9,7 +9,7 @@ prefix = 'www.balenciaga.cn'
 def parse(driver, url):
     products = []
     driver.get(url)
-    elements = util.find_elements_by_css_selector(driver, 'item-display-image-container')
+    elements = util.find_elements_by_css_selector(driver, 'a.item-display-image-container')
     for element in elements:
         products.append(element.get_attribute('href'))
     return ';'.join(products)
