9a92813cb993f8a730f8d3b774a1674aaa8959d0
yingl <linying_43151@163.com>
2017-06-26 12:02:28 +0800

    Update bally.py

--- entry_parser/bally.py
+++ entry_parser/bally.py
@@ -1,6 +1,12 @@
 # coding: utf-8
 
-prefix = 'www.bally.com'
+prefix = 'www.bally.cn'
 
 def parse(drive, url):
-  return url
+    products = []
+    driver.execute_script('window.scrollBy(0,50000)')
+    utils.sleep(3)
+    elements = utils.find_elements_by_css_selector(driver, 'js-producttile_link')
+    for element in elements:
+        products.append(element.get_attribute('href')
+    return ';'.join(products)
