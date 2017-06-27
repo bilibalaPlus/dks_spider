f97bcf5a68c967a35f79e5015cb836d66cbfbf7d
yingl <linying_43151@163.com>
2017-06-27 18:02:42 +0800

    Create chloe.py

---
+++ entry_parser/chloe.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+import sys
+sys.path.append('../')
+import util
+
+prefixes = ['www.chloe.cn']
+
+def parse(driver, url):
+    products = []
+    driver.get(url)
+    elements = util.find_elements_by_css_selector(driver, 'section.products > article.item > div > a')
+    if not elements: # https://www.chloe.cn/cn/chloe/%E5%A5%B3%E5%A3%AB/subhome/accessories_section
+        elements = util.find_elements_by_css_selector(driver, 'article.product >a')
+    for element in elements:
+        products.append(element.get_attribute('href').strip())
+    return ';'.join(products)
+
+if __name__ == '__main__':
+    driver = util.create_chrome_driver()
+    print(parse(driver, sys.argv[1]))
+    driver.quit()
