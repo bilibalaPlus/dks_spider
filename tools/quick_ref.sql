fc1b7b670e946e5a96d54f14254bdfe9778a5aa6
yingl <linying_43151@163.com>
2017-06-28 16:26:57 +0800

    Update and rename queries.sql to quick_ref.sql

--- tools/quick_ref.sql
+++ tools/quick_ref.sql
@@ -1,4 +1,5 @@
-/* entry解析 */
+/* SQL查询快速参考，因为我不会前端。。。 */
 
+/* entry解析 */
 -- 返回结果为空的入口页面
 select t1.id, t1.url, t2.content from source as t1, result as t2 where t2.content = '' and t1.url like '%chanel%' and t1.id = t2.source_id
