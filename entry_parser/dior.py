7ad38e190d5633ab74dde98f00733954ed0bb296
yingl <linying_43151@163.com>
2017-06-28 09:46:46 +0800

    Create dior.py

---
+++ entry_parser/dior.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import sys
+sys.path.append('../')
+import util
+
+prefixes = ['www.dior.cn']
+
+def parse(driver, url):
+    products = []
+    driver.get(url)
+    elements = util.find_elements_by_css_selector(driver, 'div.product > div > div > a')
+    if not elements: # http://www.dior.cn/beauty/zh_cn/%E9%A6%99%E6%B0%9B%E4%B8%8E%E7%BE%8E%E5%AE%B9/%E5%BD%A9%E5%A6%86/%E7%9C%BC%E9%83%A8/%E7%9C%BC%E5%BD%B1/fr-eyeshadows-%E7%9C%BC%E5%BD%B1.html
+        elements = util.find_elements_by_css_selector(driver, 'div.column > div.push-pic > a')
+    for element in elements:
+        products.append(element.get_attribute('href').strip())
+    return ';'.join(products)
+
+if __name__ == '__main__':
+    driver = util.create_chrome_driver()
+    print(parse(driver, sys.argv[1]))
+    driver.quit()
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security 
