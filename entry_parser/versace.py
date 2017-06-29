f706b9b99e3524e57f8ccf7d0b2e837f3a700018
yingl <linying_43151@163.com>
2017-06-29 14:33:17 +0800

    Create versace.py

---
+++ entry_parser/versace.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+import sys
+
+sys.path.append('../')
+import util
+
+prefixes = ['http://www.versace.cn']
+def parse(driver, url):
+    products = []
+    driver.get(url)
+    for i in range(10): # 确保页面拉到最下面，所有商品得到展示。
+        driver.execute_script('window.scrollBy(0, 10000)')
+        util.sleep(1)
+    elements = util.find_elements_by_css_selector(driver, 'div.js-product-image > a.js-producttile_link')
+    for element in elements:
+        products.append(element.get_attribute('href').strip())
+    return ';'.join(products)
+
+if __name__ == '__main__':
+    driver = util.create_chrome_driver()
+    print(parse(driver, sys.argv[1]))
+    driver.quit()
